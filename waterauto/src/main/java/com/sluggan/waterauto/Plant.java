d1c916cdbf4976fac03c8f0d5c9c0726d4d92fa7
mrsluggan <ericflyger@gmail.com>
2024-10-03 13:00:11 +0200

    plant.java

---
+++ waterauto/src/main/java/com/sluggan/waterauto/Plant.java
@@ -0,0 +1,44 @@
+package com.sluggan.waterauto;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "plants")
+public class Plant {
+
+    @Id
+    private String id;
+    private String name;
+    private int value;
+
+    public Plant(String id, String name, int value) {
+        this.id = id;
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+}
