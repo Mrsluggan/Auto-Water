1cd388f46b55a6a3739c1d1296ebd08bda2390d7
mrsluggan <ericflyger@gmail.com>
2024-10-03 12:53:42 +0200

    first comit

---
+++ waterauto/src/main/java/com/sluggan/waterauto/WaterautoApplication.java
@@ -0,0 +1,13 @@
+package com.sluggan.waterauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterautoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterautoApplication.class, args);
+	}
+
+}
---
+++ waterauto/src/test/java/com/sluggan/waterauto/WaterautoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sluggan.waterauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterautoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
