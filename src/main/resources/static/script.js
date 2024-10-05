58a2d2a7a9d86cb0729f73189e8dba3012f290fc
mrsluggan <ericflyger@gmail.com>
2024-10-05 18:36:22 +0200

    update

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,11 +1,5 @@
-
 setInterval(function () {
     window.location.reload();
     console.log("fuck");
 
 }, 180000);
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    console.log('Nattläge aktiverat');
-} else {
-    console.log('Ljust läge aktiverat');
-}
